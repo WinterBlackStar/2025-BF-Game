b18d813e765010ff6ceed2541309f5fa9bc8607b
WinterBlackStar <khanz3007@masseyhigh.school.nz>
2025-05-15 14:59:37 +1200

    trying to increase num rounds/infinite mode

--- B_01_Basic_Facts.py
+++ B_01_Basic_Facts.py
@@ -25,24 +25,105 @@ def string_checker(question, valid_ans=("yes", "no")):
         print(error)
         print()
 
+def mode_checker(question, valid_ans=("e", "m", "h")):
+
+    error = f"please enter a valid option from the following list: {valid_ans}"
+
+    while True:
+
+        # get user response and make sure it's lowercase
+        user_response = input(question).lower()
+
+        for item in valid_ans:
+            # check if the user response is a word in the list
+            if item == user_response:
+                return item
+    
+            # check if the user response is the same as
+            # the first letter of an item in the list
+            elif user_response == item [0]:
+                return item
+
+        # print error if user does not enter something that is valid
+        print(error)
+        print()
+
+def int_check(question):
+    while True:
+        error = "Please enter an integer that is 1 or more"
+
+        to_check = input(question)
+
+        # check for infinite mode
+        if to_check == "":
+            return "infinite"
+
+        try:
+            response = int(to_check)
+    
+            # checks that the number is more than / equal to 13
+            if response < 13:
+                print(error)
+            else:
+                return response
+            
+        except ValueError:
+            print(error)
+
+
 # Display instructions
+mode = "regular"
+rounds_played = 0
+
 def instructions():
         print("""
  
  *** Instructions ***
  
-To begin, choose the level of the questions and either customise 
-the game parameters or go with the default game (where the 
-level starts from level 1 and increases).
+To begin, choose the level of the questions easy (e),
+medium (m), hard (h), easy has plus and minus, 
+medium has times and hard has divition questions.
           
-Then choose how many rounds you'd like to play <enter> for 
-infinite mode.
+Then choose how many rounds you'd like to play (push <enter> for 
+infinite mode).
           
 Your goal is to try to answer all the questions without 
 losing any rounds.
           
  Good luck!
 """)
+
+# Initialise game variables
+
+def int_check(question):
+    while True:
+        error = "Please enter an integer that is 1 or more"
+
+        to_check = input(question)
+
+        # check for infinite mode
+        if to_check == "":
+            return "infinite"
+
+        try:
+            response = int(to_check)
+    
+            # checks that the number is more than / equal to 13
+            if response < 1:
+                print(error)
+            else:
+                return response
+            
+        except ValueError:
+            print(error)
+
+# Main Routine Starts here
+# Initialise game variables
+
+mode = "regular"
+rounds_played = 0
+
+bf_list = ["number answers"]
 
 
 # Main routine
@@ -58,26 +139,40 @@ want_instructions = string_checker("Do you want to see the instructions?")
 if want_instructions == "yes":
     instructions()
 
-print()
-print("program continues")
-
 
 # Ask user for number of rounds / infinite mode
+num_rounds = int_check("How many rounds would you like? Push <enter> for inifinte mode: ")
 
+
+if num_rounds == "infinite":
+    mode = "infinite"
+    num_rounds = 5
 
 # Game loop starts here
-
+while rounds_played < num_rounds:
 
     # Round headings
-    
+    if mode == "infinite":
+        rounds_heading = f"\n000 rounds {rounds_played + 1} of {num_rounds} (Infinite Mode) 000"
+    else:
+        rounds_heading = f"\n 💿💿💿 round {rounds_played + 1} of {num_rounds} 💿💿💿"
+
+    print(rounds_heading)
+    print()
 
     # get user choice
-    
+    user_choice = input("Choose: ")
 
     # if user choice is the exit code, break the loop
-    
+    if user_choice == "xxx":
+        break
 
-    # randomly choose from the list (excluding the exit code)
+
+    rounds_played += 1
+
+    # if users are in infinite mode, increase number of rounds!
+    if mode == "inifinte":
+        num_rounds += 1
 
 
 # Game loop ends here
