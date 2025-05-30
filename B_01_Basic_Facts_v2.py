c26976b97bd56b6042ba1d983d3db87296a081fa
WinterBlackStar <khanz3007@masseyhigh.school.nz>
2025-05-30 13:29:39 +1200

    Testing how to select mode and generate random number
    have debugging to fix

--- B_01_Basic_Facts_v2.py
+++ B_01_Basic_Facts_v2.py
@@ -1,6 +1,9 @@
 import math
 import random
 
+random_num = random.randint(1, 10)
+
+
 # Check that users have entered a valid
 # option based on a list
 def string_checker(question, valid_ans=("yes", "no")):
@@ -27,6 +30,7 @@ def string_checker(question, valid_ans=("yes", "no")):
         print()
 
 
+def mode_selection(question, valid_ans=("easy", "medium", "hard")):
 
     error = f"please enter a valid option from the following list: {valid_ans}"
 
@@ -49,28 +53,6 @@ def string_checker(question, valid_ans=("yes", "no")):
         print(error)
         print()
 
-def mode_checker(question, valid_ans=("easy", "medium", "hard")):
-
-    error = f"please enter a valid option from the following list: {valid_ans}"
-
-    while True:
-
-        # get user response and make sure it's lowercase
-        user_response = input(question).lower()
-
-        for item in valid_ans:
-            # check if the user response is a word in the list
-            if item == user_response:
-                return item
-    
-            # check if the user response is the same as
-            # the first letter of an item in the list
-            elif user_response == item [0]:
-                return item
-
-        # print error if user does not enter something that is valid
-        print(error)
-        print()
 
 def int_check(question):
     while True:
@@ -115,34 +97,12 @@ losing any rounds.
  Good luck!
 """)
 
-def int_check(question):
-    while True:
-        error = "Please enter an integer that is 1 or more"
-
-        to_check = input(question)
-
-        # check for infinite mode
-        if to_check == "":
-            return "infinite"
-
-        try:
-            response = int(to_check)
-    
-            # checks that the number is more than / equal to 13
-            if response < 1:
-                print(error)
-            else:
-                return response
-            
-        except ValueError:
-            print(error)
-
 # calculate easy mode questions
 
 # Main Routine Starts here
 # Initialise game variables
 mode = "regular"
-rounds_played = 0
+rounds_played = 1
 
 # Main Routine
 print()
@@ -157,9 +117,26 @@ want_instructions = string_checker("Do you want to see the instructions?")
 if want_instructions == "yes":
     instructions()
 
+
 # Display Mode selection
+
 print("\nenter mode selection easy (e), medium (m), hard (h)")
-mode_selection = mode_checker("\nwhat mode would you like?")
+mode_selection = input("\nwhat mode would you like?")
+
+
+    # if user chooses easy, display questio
+    
+if mode_selection =="e":
+        print("you selected e")
+
+    # if user chooses medium, display question
+elif mode_selection =="m":
+        print("you selected m")
+
+    # if user chooses hard, display question
+elif mode_selection =="h":
+        print("you selected h")
+
 
 # Ask user for number of rounds / infinite mode
 num_rounds = int_check("How many rounds would you like? Push <enter> for inifinte mode: ")
@@ -174,33 +151,24 @@ while rounds_played < num_rounds:
 
     # Round headings
     if mode == "infinite":
-        rounds_heading = f"\n000 rounds {rounds_played} of {num_rounds}(Infinite Mode) 000"
+        rounds_heading = f"\n000 rounds {rounds_played + 1} (Infinite Mode) 000"
+        # if users are in infinite mode, increase number of rounds!
+        num_rounds += 1
     else:
-        rounds_heading = f"\n 💿💿💿 round {rounds_played} of {num_rounds} 💿💿💿"
+        rounds_heading = f"\n 💿💿💿 round {rounds_played + 1} of {num_rounds} 💿💿💿"
 
     print(rounds_heading)
     print()
 
-    # get user choice
-    user_choice =input()
+# get user choice
+    user_choice = string_checker("Choose:", bf_list)
+    print("you chose", user_choice)
 
-    # randomly choose from the rps list (excluding the exit code)
-
-    rounds_played += 1       
-
-
-    # if user choice is the exit code, break the loop
+# if user choice is the exit code, break the loop
     if user_choice == "xxx":
-        # set end_game to beak outer loop
-        end_game = "yes"
         break
-
-
-    # if users are in infinite mode, increase number of rounds!               
-    if mode == "inifinte":
-        num_rounds += 1
-
 
 # Game loop ends here
 
 # Game History / Statistics area
+
